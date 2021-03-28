fef800bf8f94f253e7f59ab908d0bb87292d0da2
Rafael Medeiros <rafaelgurudesign@gmail.com>
2021-03-27 23:09:24 -0300

    add types

--- src/walletsdkcpp/types.h
+++ src/walletsdkcpp/types.h
@@ -1,4 +1,52 @@
 #ifndef WALLET_SDK_CPP_TYPES_H
 #define WALLET_SDK_CPP_TYPES_H
 
+#include <string>
+#include <vector>
+
+struct DepositItem
+{
+    int ID;
+    std::string Address;
+    std::string Amount;
+    std::string Blockhash;
+    int Confirmations;
+    int WalletID;
+    int CompanyID;
+    std::string Txid;
+    std::string Currency;
+    std::string Status;
+};
+
+struct DepositPagesQuery
+{
+    int PerPage;
+    int Page;
+    DepositQuery Query;
+    DepositOrderBy OrderBy;
+};
+
+struct DepositQuery
+{
+    int To;
+    int From;
+    int UpdatedTo;
+    int UpdatedFrom;
+    std::string Currency;
+    int CompanyID;
+    int WalletID;
+};
+struct DepositOrderBy
+{
+    std::string Field;
+    std::string Direction;
+};
+
+struct DepositPagesResponse
+{
+    std::vector<DepositItem> Data;
+    int Page;
+    int Pages;
+    int Total;
+};
 #endif
